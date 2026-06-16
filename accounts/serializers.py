5b29af27ebd3fb07a11efa8208f31e89fecd89c9
Macho <nonsonnabugwu911@gmail.com>
2026-06-16 19:07:29 +0100

    user registration

---
+++ accounts/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import User
+
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only = True)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import User
+from .serializers import *
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+class RegisterView(APIView):
+    authentication_classes = []
+    permission_classes = []
+    def get(self, request):
+        serializer = RegisterSerializer(data=request.data)
+
+        try:
+            user = User.objects.create_user(
+                username = serializer.validated_data['username'],
+                password = serializer.validated_data['password']
+            )
+
+        except user.exists():
+            return Response({
+                'error': 'User already exists'
+            }, status=status.HTTP_401_UNAUTHORIZED)
