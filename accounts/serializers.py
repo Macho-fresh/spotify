a5f149bd747fb276f2bb4716f2a5a3a51f04eb23
Macho <nonsonnabugwu911@gmail.com>
2026-06-17 19:20:37 +0100

    user login

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,4 +3,9 @@ from .models import User
 
 class RegisterSerializer(serializers.Serializer):
     username = serializers.CharField()
+    email = serializers.EmailField()
     password = serializers.CharField(write_only = True)
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only = True)    
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from .serializers import *
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.contrib.auth import authenticate, login
 
 class RegisterView(APIView):
     authentication_classes = []
@@ -14,6 +15,7 @@ class RegisterView(APIView):
         try:
             user = User.objects.create_user(
                 username = serializer.validated_data['username'],
+                email = serializer.validated_data['email'],
                 password = serializer.validated_data['password']
             )
 
@@ -21,3 +23,25 @@ class RegisterView(APIView):
             return Response({
                 'error': 'User already exists'
             }, status=status.HTTP_401_UNAUTHORIZED)
+
+class LoginView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request):
+        serializer = LoginSerializer(data=request.data)
+        
+        username = serializer.validated_data['username'],
+        password = serializer.validated_data['password']
+
+        try:
+            user = authenticate(request, username=username, password=password)
+        except user is None:
+            return Response({
+                'error': 'user does not exist'
+            })
+        
+        login(request, user)
+
+        
+            
